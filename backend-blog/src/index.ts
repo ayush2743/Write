ab90c348a82816b073b9c4ee03a39f99c8b09cbb
ayush2743 <saxenaayush27.work@gmail.com>
2024-12-04 18:18:14 +0530

    added cors

--- backend-blog/src/index.ts
+++ backend-blog/src/index.ts
@@ -1,9 +1,7 @@
 import { Hono } from 'hono';
-import { sign, verify } from 'hono/jwt';
-
-
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
+import { cors } from 'hono/cors'
 
 
 // Create the main Hono app
@@ -16,6 +14,8 @@ const app = new Hono<{
     userID: string
   }
 }>();
+
+app.use('/', cors());
 
 app.route('api/v1/user', userRouter);
 app.route('api/v1/blog', blogRouter);
