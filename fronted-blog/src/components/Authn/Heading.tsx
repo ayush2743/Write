ddb6ae4f85c3b9fb5343abdb1a935e9575c5bedb
ayush2743 <saxenaayush27.work@gmail.com>
2025-01-05 01:25:20 +0530

    updated css for auth heading

--- fronted-blog/src/components/Authn/Heading.tsx
+++ fronted-blog/src/components/Authn/Heading.tsx
@@ -2,6 +2,6 @@
 export default function Heading({ title }: { title: string }) {
 
     return (
-        <h1 className="mb-8 text-5xl sm:text-6xl font-bold text-left font-serif">{title}</h1>
+        <h1 className="drop-shadow-[2px_2px_4px_rgba(179,214,235,0.5)] mb-8 text-5xl sm:text-6xl font-bold text-left font-serif">{title}</h1>
     );
 }
